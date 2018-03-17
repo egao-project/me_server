6807f9697be4eaa473b1993a483df3b18898082a
ushikit <ushikit@egao-inc.co.jp>
2018-03-17 21:27:37 +0900

    フレーム検索できるようにした

--- me_api/serializer.py
+++ me_api/serializer.py
@@ -3,11 +3,12 @@ from rest_framework import serializers
 from .models import Frame, Picture
 
 class FrameSerializer(serializers.ModelSerializer):
+    pictures = serializers.StringRelatedField(many=True)
     class Meta:
         model = Frame
-        fields = ('username','title','frame_type')
+        fields = ('username','title','frame_type','pictures')
 
 class PictureSerializer(serializers.ModelSerializer):
     class Meta:
         model = Picture
-        fields = ('frame','name','position')
+        fields = ('name','position')
